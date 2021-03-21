443109723d014673053e17c10b2e3cdd574c7d74
Phoebe Hu <phoebehu@phoebes-mbp.lan>
2021-03-21 14:33:49 -0700

    change in ArticleListItem

--- src/ArticleListItem/ArticleListItem.jsx
+++ src/ArticleListItem/ArticleListItem.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ArticleListItem = (props) => {
   const { item } = props;
   return (
     <section>
-      <h2>{item.title} </h2>
+      <h2>
+        {' '}
+        <Link to={'articlelist/' + item.slug}>{item.title} </Link>
+      </h2>
       <time dateTime={item.timeStamp}> {item.displayDate} </time>
       <p>{item.shortText}</p>
     </section>
